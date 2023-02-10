42c9092d007da354a29e84d551a40e28a4922a48
zotin <zotinalexander@gmail.com>
2023-02-10 16:07:03 +0100

    commit

---
+++ src/test/java/com/example/homework7februar/Homework7FebruarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework7februar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework7FebruarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
